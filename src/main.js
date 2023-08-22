85b6b73e33699a3bcbfe9d82e5511c52e9aef2da
yihao.sun <15941999082@163.com>
2023-08-22 16:36:13 +0800

    [feat]: Element Plus 导入(按需)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+// 完整引用 (打包的文件可能会大一些)
+// app.use(ElementPlus)
+app.mount('#app')
