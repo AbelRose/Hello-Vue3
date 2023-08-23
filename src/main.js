71b114ed71765be846af14c8ba2b5e0050e052de
yihao.sun <15941999082@163.com>
2023-08-23 17:15:22 +0800

    [feat]: vue3 引入 router

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import './style.css'
 import App from './App.vue'
 // import ElementPlus from 'element-plus'
 // import 'element-plus/dist/index.css'
+import router from './router'
 
 const app = createApp(App)
 // 完整引用 (打包的文件可能会大一些)
 // app.use(ElementPlus)
+app.use(router)
 app.mount('#app')
