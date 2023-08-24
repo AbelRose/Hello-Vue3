354115578880f9c3ee96ce054a7117ec18e72f6a
yihao.sun <15941999082@163.com>
2023-08-24 16:53:51 +0800

    [feat]: 下拉框 dropdown

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -10,6 +10,30 @@
             </el-button>
         </div>
         <div class="r-content">
+            <el-dropdown>
+                <span class="el-dropdown-link">
+                    <img class="user" src="../assets/images/user.png" alt="">
+                </span>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>个人中心</el-dropdown-item>
+                        <el-dropdown-item>退出</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
         </div>
     </el-header>
 </template>
+
+<script>
+</script>
+
+<style lang="less" scoped>
+.r-content {
+    .user {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+    }
+}
+</style>
