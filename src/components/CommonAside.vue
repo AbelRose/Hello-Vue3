0e89186ccc873539ce4065e2ed6a81de5045ed27
yihao.sun <15941999082@163.com>
2023-08-25 11:07:18 +0800

    [feat]: 动态图标

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -1,30 +1,97 @@
 <template>
     <el-aside width="200px">
-        <el-menu class="el-menu-vertical-demo">
-            <el-menu-item index="2">
-                <el-icon><icon-menu /></el-icon>
-                <span>Navigator Two</span>
+        <el-menu class="el-menu-vertical-demo" background-color="#545c64" text-color="#fff" :collapse="false">
+            <el-menu-item :index="item.path" v-for="item in noChildren()" :key="item.path">
+                <!-- 动态 icon 为什么可以这样写 因为在main.js里面已经把需要的东西注册进来了 可以动态使用-->
+                <component class="icons" :is="item.icon">
+                </component>
+                <span>{{ item.label }}</span>
             </el-menu-item>
-            <el-sub-menu index="1">
+            <el-sub-menu :index="item.label" v-for="item in hasChildren()" :key="item.path">
                 <template #title>
-                    <el-icon>
-                        <location />
-                    </el-icon>
-                    <span>Navigator One</span>
+                    <component class="icons" :is="item.icon">
+                    </component>
+                    <span>{{ item.label }}</span>
                 </template>
-                <el-menu-item-group title="Group One">
-                    <el-menu-item index="1-1">item one</el-menu-item>
-                    <el-menu-item index="1-2">item two</el-menu-item>
+                <el-menu-item-group>
+                    <el-menu-item :index="subItem.path" v-for="( subItem, subIndex ) in item.children" :key="subIndex">
+                        <component class="icons" :is="subItem.icon">
+                        </component>
+                        <span>{{ subItem.label }}</span>
+                    </el-menu-item>
                 </el-menu-item-group>
-                <el-menu-item-group title="Group Two">
-                    <el-menu-item index="1-3">item three</el-menu-item>
-                </el-menu-item-group>
-                <el-sub-menu index="1-4">
-                    <template #title>item four</template>
-                    <el-menu-item index="1-4-1">item one</el-menu-item>
-                </el-sub-menu>
             </el-sub-menu>
         </el-menu>
     </el-aside>
 </template>
+
+<script>
+export default {
+    // Vue3和Vue2的区别 setUp之后 return出去
+    // Vue2是data
+    setup() {
+        const list = [
+            {
+                path: '/',
+                name: 'home',
+                label: '首页',
+                icon: 'house',
+                url: 'Home/Home'
+            },
+            {
+                path: '/mall',
+                name: 'mall',
+                label: '商品管理',
+                icon: 'video-play',
+                url: 'MallManage/MallManage'
+            },
+            {
+                path: '/user',
+                name: 'user',
+                label: '用户管理',
+                icon: 'user',
+                url: 'UserManage/UserManage'
+            },
+            {
+                path: '/other',
+                label: '其他',
+                icon: 'location',
+                children: [
+                    {
+                        path: '/page1',
+                        name: 'page1',
+                        label: '页面1',
+                        icon: 'setting',
+                        url: 'Other/PageOne'
+                    },
+                    {
+                        path: '/page2',
+                        name: 'page2',
+                        label: '页面2',
+                        icon: 'setting',
+                        url: 'Other/PageTwo'
+                    }
+                ]
+            }
+        ];
+        const noChildren = () => {
+            return list.filter((item) => !item.children)
+        };
+        const hasChildren = () => {
+            return list.filter((item) => item.children)
+        };
+        return {
+            noChildren,
+            hasChildren
+        };
+    },
+};
+</script>
+
+<style lang="less" scoped>
+.icons {
+    width: 18px;
+    height: 18px;
+}
+</style>
 
