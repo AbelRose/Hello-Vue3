7d201befecadb970c2e97cc0c0dcb840f5c4a966
yihao.sun <15941999082@163.com>
2023-08-25 17:09:06 +0800

    [feat]: 组件路由跳转

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -3,22 +3,26 @@
         <el-menu class="el-menu-vertical-demo" background-color="#545c64" text-color="#fff"
             :collapse="!$store.state.isCollapse" :collapse-transition="false">
             <!-- :collapse="!$store" 注意没有this  :collapse-transition false 是去掉动画 -->
-            <el-menu-item :index="item.path" v-for=" item  in  noChildren() " :key="item.path">
+            <h3 v-show="$store.state.isCollapse">Hello Vue3</h3>
+            <h3 v-show="!$store.state.isCollapse">Vue3</h3>
+            <el-menu-item :index="item.path" v-for=" item  in  noChildren() " :key="item.path" @click="clickMenu(item)">
                 <!-- 动态 icon 为什么可以这样写 因为在main.js里面已经把需要的东西注册进来了 可以动态使用-->
-                <component class="icons" :is="item.icon">
-                </component>
+                <component class="icons" :is="item.icon"></component>
                 <span>{{ item.label }}</span>
             </el-menu-item>
             <el-sub-menu :index="item.label" v-for=" item  in  hasChildren() " :key="item.path">
                 <template #title>
-                    <component class="icons" :is="item.icon">
-                    </component>
+                    <!-- TODO 为什么这个地方icon要写死 不能动态获取 否则的话折叠的时候不显示 -->
+                    <el-icon>
+                        <location />
+                    </el-icon>
+                    <!-- <component class=" icons" :is="item.icon"></component> -->
                     <span>{{ item.label }}</span>
                 </template>
                 <el-menu-item-group>
-                    <el-menu-item :index="subItem.path" v-for="(  subItem, subIndex  ) in  item.children " :key="subIndex">
-                        <component class="icons" :is="subItem.icon">
-                        </component>
+                    <el-menu-item :index="subItem.path" v-for="(  subItem, subIndex  ) in  item.children " :key="subIndex"
+                        @click="clickMenu(subItem)">
+                        <component class="icons" :is="subItem.icon"></component>
                         <span>{{ subItem.label }}</span>
                     </el-menu-item>
                 </el-menu-item-group>
@@ -28,6 +32,8 @@
 </template>
 
 <script>
+import { useRouter } from 'vue-router'
+
 export default {
     // Vue3和Vue2的区别 setUp之后 return出去
     // Vue2是data
@@ -43,6 +49,7 @@ export default {
             {
                 path: '/other',
                 label: '其他',
+                name: 'other',
                 icon: 'location',
                 children: [
                     {
@@ -69,9 +76,16 @@ export default {
         const hasChildren = () => {
             return list.filter((item) => item.children)
         };
+        const router = useRouter();
+        const clickMenu = (item) => {
+            router.push({
+                name: item.name,
+            })
+        };
         return {
             noChildren,
-            hasChildren
+            hasChildren,
+            clickMenu
         };
     },
 };
@@ -85,6 +99,12 @@ export default {
 
 .el-menu {
     border-right: none;
+
+    h3 {
+        line-height: 48px;
+        color: #fff;
+        text-align: center;
+    }
 }
 </style>
 
--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,29 @@ const routes = [
     {
         path: '/',
         component: () => import('../views/Main.vue'),
+        // 重定向到home
+        redirect: '/home',
         children: [
             {
-                path: '/',
+                path: '/home',
                 name: 'home',
                 component: () => import('../views/home/Home.vue')
-            }
+            },
+            {
+                path: '/user',
+                name: 'user',
+                component: () => import('../views/user/User.vue')
+            },
+            {
+                path: '/page1',
+                name: 'page1',
+                component: () => import('../views/Page1.vue')
+            },
+            {
+                path: '/page2',
+                name: 'page2',
+                component: () => import('../views/Page2.vue')
+            },
         ]
     }
 ]
