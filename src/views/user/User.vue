ac0ae81df5c14343a11f032a0afb4dfdbd8416d7
yihao.sun <yihao.sun@accenture.com>
2023-09-12 21:59:13 +0800

    [feat]: 用户分页(有问题)

--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="table">
-        <el-table :data="list" style="width: 100%" height="650px">
+        <el-table :data="list" style="width: 100%" height="600px">
             <el-table-column v-for="item in tableLabel" :key="item.prop" :label="item.label" :prop="item.prop"
                 :width="item.width ? item.width : 125" />
             <el-table-column fixed="right" label="操作" min-width="180">
@@ -10,6 +10,10 @@
                 </template>
             </el-table-column>
         </el-table>
+        <br />
+        <!-- 分页 -->
+        <el-pagination small background layout="prev, pager, next" :total="config.total" @current-change="changePage"
+            class="pager mt-4" />
     </div>
 </template>
 
@@ -19,6 +23,7 @@ import { defineComponent, getCurrentInstance, onMounted, ref, reactive } from 'v
 export default defineComponent({
     setup() {
         const { proxy } = getCurrentInstance()
+        // ref 和 tableLabel做双向绑定
         const list = ref([])
         const tableLabel = reactive([
             {
@@ -45,18 +50,26 @@ export default defineComponent({
             },
         ]);
         onMounted(() => {
-            getUserData()
-        })
-        const getUserData = async () => {
-            let res = await proxy.$api.getUserData();
+            getUserData(config)
+        });
+        const config = reactive({
+            total: 0,
+            page: 1
+        });
+        const getUserData = async (config) => {
+            let res = await proxy.$api.getUserData(config);
+            config.total = res.count
             list.value = res.list.map((item) => {
                 item.sexLabel = item.sex === 0 ? '女' : '男 '
                 return item
             })
         };
+        const changePage = (page) => {
+            config.page = page
+            getUserData(config)
+        };
         return {
-            list,
-            tableLabel
+            list, tableLabel, config, changePage
         }
     },
 })
